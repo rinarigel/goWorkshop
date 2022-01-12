34aef26575f47fd4d97100ec16c6d2ad0b8b6412
ibobrova <ibobrova@ozon.ru>
2022-01-12 08:52:38 +0400

    update functions

--- functions.go
+++ functions.go
@@ -1,24 +1,32 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 )
 
 func main() {
-	printCircleArea(2)
-	printCircleArea(5)
+	printCircleArea(-2)
 }
 
 func printCircleArea(radius int) {
+	circleArea, err := calculatedCircleArea(radius)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
 
 	fmt.Printf("Circle radius: %d cm\n", radius)
 	fmt.Println("Formula for calculating area of circle: S=πr2")
 
-	circleArea := calculatedCircleArea(radius)
 	fmt.Printf("Circle area: %.2f cm. kv\n", circleArea)
 }
 
-func calculatedCircleArea(radius int) float32 {
+func calculatedCircleArea(radius int) (float32, error) {
+	if radius <= 0 {
+		return float32(0), errors.New("Radius of circle can't be negative")
+	}
+
 	const pi = 3.1415
-	return float32(radius) * float32(radius) * pi
+	return float32(radius) * float32(radius) * pi, nil
 }
