f7b8c0eaf723e78dd12d3af1ee7fc3226e0bf682
ibobrova <ibobrova@ozon.ru>
2022-01-13 22:17:11 +0300

    for break

--- massive.go
+++ massive.go
@@ -5,14 +5,13 @@ import (
 )
 
 func main() {
-	var todoList = [3]string{"купить хлеб"}
+	i := 0
+	for {
+		if i == 100 {
+			break
+		}
 
-	todoList[1] = "купить молоко"
-	todoList[2] = "купить пиво"
-
-	fmt.Printf("К-во элементов в списке: %d\n", len(todoList))
-
-	for _, item := range todoList {
-		fmt.Printf("%s\n", item)
+		fmt.Println(i)
+		i++
 	}
 }
