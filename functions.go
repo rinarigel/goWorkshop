c8a5d6e8f18a1c5d605bcd3b86110138fa36e5a7
ibobrova <ibobrova@ozon.ru>
2022-01-13 15:08:21 +0400

    calculated area and square for triangle

--- functions.go
+++ functions.go
@@ -6,21 +6,21 @@ import (
 )
 
 func main() {
-	printResult(0.6, 1)
+	printResult(0.6, 1, 1)
 }
 
-func printResult(length float32, width float32) {
+func printResult(length float32, width float32, height float32) {
 	rectangleArea, err := calculatedRectangleArea(length, width)
 	if err != nil {
 		fmt.Println(err.Error())
 		return
 	}
 
-	fmt.Printf("length: %d cm\n", length)
-	fmt.Printf("width: %d cm\n", width)
-	fmt.Println("Formula for calculating area of rectangle: S=2(length+width)")
+	fmt.Printf("Длина: %.2f см\n", length)
+	fmt.Printf("Ширина: %.2f см\n", width)
+	fmt.Println("Формула для подсчета периметра прямоугольника: S=2(length+width)")
 
-	fmt.Printf("Rectangle area: %.2f cm. kv\n", rectangleArea)
+	fmt.Printf("Периметр прямоугольника: %.2f cm. kv\n", rectangleArea)
 
 	rectangleSquare, err := calculatedRectangleSquare(length, width)
 	if err != nil {
@@ -28,9 +28,31 @@ func printResult(length float32, width float32) {
 		return
 	}
 
-	fmt.Println("Formula for calculating square of rectangle: S=length*width")
+	fmt.Println("Формула для подсчета площади прямоугольника: S=length*width")
 
-	fmt.Printf("Rectangle square: %.2f cm. kv\n", rectangleSquare)
+	fmt.Printf("Площадь прямоугольника: %.2f cm. kv\n", rectangleSquare)
+
+	triangleArea, err := calculatedTriangleArea(length, height)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	fmt.Printf("Длина: %.2f см\n", length)
+	fmt.Printf("Высота: %.2f см\n", height)
+	fmt.Println("Формула для подсчета периметра треугольника: S=0.5*height*length")
+
+	fmt.Printf("Периметр треугольника: %.2f cm. kv\n", triangleArea)
+
+	triangleSquare, err := calculatedTriangleSquare(length, width)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	fmt.Println("Формула для подсчета площади треугольника: S=length*width*0.5")
+
+	fmt.Printf("Площадь треугольника: %.2f cm. kv\n", triangleSquare)
 }
 
 func calculatedRectangleArea(length, width float32) (float32, error) {
@@ -47,4 +69,20 @@ func calculatedRectangleSquare(length, width float32) (float32, error) {
 	}
 
 	return float32(length * width), nil
+}
+
+func calculatedTriangleArea(length, height float32) (float32, error) {
+	if (length <= 0) || (height <= 0) {
+		return float32(0), errors.New("Sides can't be negative!")
+	}
+
+	return float32(length*height) * 0.5, nil
+}
+
+func calculatedTriangleSquare(length, width float32) (float32, error) {
+	if (length <= 0) || (width <= 0) {
+		return float32(0), errors.New("Sides can't be negative!")
+	}
+
+	return float32(length*width) * 0.5, nil
 }
